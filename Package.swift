ac1894d6d4fb834b44803d466441202b15a90a6d
mat2uken <ku@yasagure.jp>
2022-04-22 22:52:09 +0900

    test commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import Foundation
+import PackageDescription
+
+let file = "WebRTC-97.4692.4.0/WebRTC.xcframework.zip"
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "WebRTC", targets: ["WebRTC"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/shiguredo/sora-ios-sdk-specs/releases/download/\(file)",
+            checksum: "f5bb3bc266f63616a4c608ca4dd5396bc084f2b8e99d4f7d756235e2e0d9afef"
+        ),
+    ]
+)
